0253277fb6074fad61c01eb9766699621f412989
andrzej-falkowski <andrzej.falkowski88@gmail.com>
2024-05-05 00:47:50 +0200

    Added logic to create a gallery, added Notiflix and SimpleLightbox. Finished CSS styles

--- src/js/pixabayApi.js
+++ src/js/pixabayApi.js
@@ -1,80 +1,25 @@
 'use strict';
 
 import axios from 'axios';
-// export {fetchImages};
+export {searchImages};
 
 const apiKey = '43601558-493cea9937d74fa86e08084b6';
-const baseUrl = 'https://pixabay.com/api/';
-const searchForm = document.querySelector('form#search-form');
 
-const queryParams = 'image_type=photo&safesearch=true&orientation=horizontal';
-// const searchForm = document.querySelector('search-form');
-const inputData = searchForm.elements['searchQuery'];
-// const inputData = document.getElementById('search-input');
+async function searchImages(searchQuery, pageNum, perPage) {
+  const params = new URLSearchParams({
+    key: apiKey,
+    orientation: 'horizontal',
+    image_type: 'photo',
+    safesearch: true,
+    per_page: perPage,
+    page: pageNum,
+    q: searchQuery,
+  });
 
-const loadMore = document.querySelector('.load-more');
-
-
-const submit = document.querySelector('form#search-form button[type=submit]');
-const resultsGallery = document.querySelector('.gallery');
-
-let searchQuery = ""
-let page = 1;
-
-
-
-export async function searchImages() {
-  searchQuery = inputData.value;
-  console.log(inputData.value)
   const response = await axios.get(
-    `${baseUrl}?key=${apiKey}&${queryParams}&q=${searchQuery}`);
-const data = await [response.data];
-console.log(response.data);
-
-// if (page === 1){
-//   resultsGallery.innerHTML = ""
-// }
-
-resultsGallery.innerHTML = data.map((result) => {
-  `  <div class="photo-card">
-  <img src="${result.previewURL}" alt="" loading="lazy" />
-  <div class="info">
-    <p class="info-item">
-      <b>Likes</b>
-    </p>
-    <p class="info-item">
-      <b>Views</b>
-    </p>
-    <p class="info-item">
-      <b>Comments</b>
-    </p>
-    <p class="info-item">
-      <b>Downloads</b>
-    </p>
-  </div>
-  </div>`
-}).join("")
-
-page++
-if (page > 1){
-loadMore.style.display = 'block';
+    `https://pixabay.com/api/?${params}`
+  );
+  const data = response.data;
+  console.log(data);
+  return data;
 }
-}
-
-searchForm.addEventListener('submit', (event) => {
-  event.preventDefault();
-  page = 1;
-  searchImages()
-})
-
-// const button = document.querySelector('');
-// export async function searchImages(searchQuery) {
-// inputData = searchQuery.value
-
-//   const response = await axios.get(
-//     `${baseUrl}?key=${apiKey}&${queryParams}&q=${searchQuery}`
-//   );
-//   const data = await response.data;
-//   // console.log(data);
-//   return data;
-// }
